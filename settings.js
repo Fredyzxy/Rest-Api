14caee1419c351e779f82647095489b3af88e63d
Fredyzxy <138654587+Fredyzxy@users.noreply.github.com>
2023-07-05 23:04:44 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
 global.creator = 'Fredyzxy'  //Isi creator 
-global.MONGO_DB_URI = "mongodb+srv://fredyyzxy:<password>@fredyy.54fn2fv.mongodb.net/?retryWrites=true&w=majority"        //Isi Database Mongodb
+global.MONGO_DB_URI = "mongodb+srv://fredyyzxy:ynoktxwfwyewirye@fredyy.54fn2fv.mongodb.net/?retryWrites=true&w=majority"        //Isi Database Mongodb
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //Isi apa aja bebas
 global.your_email = "fredyyzxy@gmail.com"          //Isi Email Untuk Pengiriman Kode
 global.email_password = "ynoktxwfwyewirye"      //Isi Aplikasi Sandi Email Pengeriman Kode
